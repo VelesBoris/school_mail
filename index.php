20824b50a6f60c2e6588e685588a8a2e842e75ac
Boris_Veles <bloodhawkey@gmail.com>
2025-06-25 11:16:02 +0300

    Create form in index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send mail to school</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label>ФИО</label>
+        <input type="text" name="name" placeholder="ФИО"><br>
+        <label>Номер телефона</label>
+        <input type="phone" name="phone" placeholder="Номер телефона">
+        <label>Электронная почта</label>
+        <input type="email" name="email" placeholder="Электронная почта">
+        <label>Сообщение</label>
+        <input type="textarea" name="mess" id="mess">
+        <button type="submit">Отправить</button>
+    </form>
+</body>
+</html>
